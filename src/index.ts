be65c8b7801b7a766fbdd8d35594db9e4fcbcc5e
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2021-04-14 02:51:06 +0530

    [dev] added typeORM and mongoDB support

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-import dotenv from 'dotenv';
-import app from './app';
+import "reflect-metadata";
+import dotenv from "dotenv";
+import app from "./app";
+import { createConnection, getMongoRepository } from "typeorm";
+import { devConfig } from "../ormconfig";
+import { User } from "./entities/User";
 
 dotenv.config();
 
 const PORT: number = Number(process.env.PORT) || 8000;
 
-app.listen((PORT), () => {
-  console.log(`Server is up on ${PORT}!`);
-});
+const main = async () => {
+  createConnection(Object(devConfig))
+    .then(async (connection) => {
+      const db = connection.mongoManager;
+
+      const user = new User();
+      user.username = "sherwyn111";
+
+      await db.save(user);
+      console.log("Saved a new user with id: " + user.id);
+
+      console.log("Loading users from the db...");
+      const users = await db.find(User);
+      console.log({ users });
+
+      const userRepository = getMongoRepository(User);
+      userRepository.deleteMany({ username: "sherwyn11" });
+
+      const usersUpdated = await db.find(User);
+      console.log({ usersUpdated });
+    })
+    .catch((error) => console.log(error));
+
+  app.listen(PORT, () => {
+    console.log(`Server is up on ${PORT}!`);
+  });
+};
+
+main();
