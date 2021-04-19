d0647b1f1f7b3dddfefbf51d2512619ae95a898e
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2021-04-19 11:26:33 +0530

    [deploy] deployed to Heroku

--- dist/src/routers/test.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const testRouter = express_1.Router();
-testRouter.route("/test-route").get((_req, res) => {
-    res.status(200).send({ message: "Test Works!" });
-});
-exports.default = testRouter;
-//# sourceMappingURL=test.route.js.map
